e06d15f3e80d4f0f85d6ffea1493fc67087b864c
Pinaki Das <54094697+daspinaki@users.noreply.github.com>
2021-01-11 11:25:49 +0530

    Update ahcoder.cxx

--- coders/ahcoder.cxx
+++ coders/ahcoder.cxx
@@ -1,29 +1,4 @@
-/**
- * ahcoder.cxx
- *
- * Adaptive Huffman Coding (FGK Algorithm)
- * by snovvcrash
- * 04.2017
- */
 
-/**
- * Copyright (C) 2017 snovvcrash
- *
- * This file is part of libcoders.
- *
- * libcoders is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * libcoders is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with libcoders.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include <iostream>
 #include <fstream>
