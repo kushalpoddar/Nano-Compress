147e7c0f934c7a26ceca5ce35752de2d68e549d6
Pinaki Das <54094697+daspinaki@users.noreply.github.com>
2021-01-11 11:28:11 +0530

    Update bhcoder.hxx

--- coders/bhcoder.hxx
+++ coders/bhcoder.hxx
@@ -1,32 +1,3 @@
-/**
- * bhcoder.hxx
- *
- * Huffman Coding on Bigrams (Markov Chain with Depth 1)
- * by snovvcrash
- * 04.2017
- */
-
-/**
- * Copyright (C) 2017 snovvcrash
- *
- * This file is part of libcoders.
- *
- * libcoders is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * libcoders is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with libcoders.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* Код Хаффмана — Биграммная модель — Википедия
-	https://goo.gl/A9tLtT */
 
 #pragma once
 #ifndef BHCODER_HXX
